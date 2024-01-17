19f0252a049192fa3bea47aeddb341715c1dbfa0
Michel Ruiter <169541@student.horizoncollege.nl>
2024-01-17 15:39:24 +0100

    Werkende api uitlezing

---
+++ TvMazeAPI/TvMazeAPI/Core/Models/ShowModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TvMazeAPI.Core.Models
+{
+    public class ShowModel
+    {
+        public string Actors { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ TvMazeAPI/TvMazeAPI/Core/Models/TvMazeData.cs
@@ -0,0 +1,10 @@
+﻿using TvMazeAPI.Core;
+
+namespace TvMazeAPI.Core.Models
+{
+    public class TvMazeData
+    {
+        public string Name { get; set; }
+        //    public ShowModel Show {  get; set; }
+    }
+}
